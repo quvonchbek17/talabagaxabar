d7388657bf9b011ea07f83c700c103efac659a15
quvonchbek17 <quvonchbekmuysinov3@gmail.com>
2024-05-15 17:23:40 +0500

    add login validation

--- src/modules/auth/dto/sign.dto.ts
+++ src/modules/auth/dto/sign.dto.ts
@@ -1,14 +1,15 @@
-import { ApiProperty, OmitType } from '@nestjs/swagger';
-import { IsNumber, IsString, Length, MaxLength } from 'class-validator'
+import { IsNotEmpty, IsString, Length, Matches } from 'class-validator'
 
 export class Sign {
     @IsString()
-    @MaxLength(20)
-    @ApiProperty({ type: 'string', example: 'eshmat', required: true})
-    adminname: string;
-
+    @IsNotEmpty()
+    @Length(4, 20, { message: "Username minimum 4, maximum 20 ta belgidan iborat bo'lishi kerak" })
+    @Matches(/^[a-zA-Z0-9]+$/, { message: "Usernameda bo'sh joy(probel) ishlatish mumkin emas.Faqat raqam va harflar qatnashishi mumkin" })
+    readonly adminname: string;
+    
     @IsString()
-    @MaxLength(20)
-    @ApiProperty({ type: 'string', example: '1111', required: true })
-    password: string;
+    @IsNotEmpty()
+    @Length(4, 20, { message: "Parol minimum 4, maximum 20 ta belgidan iborat bo'lishi kerak" })
+    @Matches(/^[a-zA-Z0-9!@#$]+$/, { message: "Parolda bo'sh joy(probel) ishlatish mumkin emas. Faqat raqam, harf, (!, @, #, $) belgilari qatnashishi mumkin" })
+    readonly password: string;
 }
