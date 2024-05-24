0e46efc98e262b9da71f6930a8dbc2dee65ff27d
quvonchbek17 <quvonchbekmuysinov3@gmail.com>
2024-05-24 20:26:52 +0500

    super admin permissions

--- src/modules/permissions/permissions.controller.ts
+++ src/modules/permissions/permissions.controller.ts
@@ -9,28 +9,28 @@ import { JwtAuthGuard, HasRole } from '@guards';
 export class PermissionsController {
   constructor(private readonly permissionsService: PermissionsService) {}
 
-  @SetRoles(rolesName.developer)
+  @SetRoles(rolesName.developer, rolesName.super_admin)
   @UseGuards(JwtAuthGuard, HasRole)
   @Post("create")
   create(@Body() body: CreatePermissionDto) {
     return this.permissionsService.create(body);
   }
 
-  @SetRoles(rolesName.developer)
+  @SetRoles(rolesName.developer, rolesName.super_admin)
   @UseGuards(JwtAuthGuard, HasRole)
   @Get("all")
   get() {
     return this.permissionsService.getPermissions();
   }
 
-  @SetRoles(rolesName.developer)
+  @SetRoles(rolesName.developer, rolesName.super_admin)
   @UseGuards(JwtAuthGuard, HasRole)
   @Patch("update")
   update(@Body() body: UpdatePermissionDto) {
     return this.permissionsService.update(body);
   }
 
-  @SetRoles(rolesName.developer)
+  @SetRoles(rolesName.developer, rolesName.super_admin)
   @UseGuards(JwtAuthGuard, HasRole)
   @Delete(":id")
   remove(@Param() params: PermissionParamsIdDto) {
